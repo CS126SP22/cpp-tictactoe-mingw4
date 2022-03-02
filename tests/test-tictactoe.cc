345ae9a55b26f3620b13f9388f47bb19d34fbd8f
mingw4 <mingw4@illinois.edu>
2022-03-01 19:58:09 -0600

    TEST_CASES: "Invalid string provided to constructor" and "Boards with no winner" finished.

--- tests/test-tictactoe.cc
+++ tests/test-tictactoe.cc
@@ -7,21 +7,34 @@ using tictactoe::Board;
 using tictactoe::BoardState;
 
 TEST_CASE("Invalid string provided to constructor") {
-  SECTION("String is too short") {
-    REQUIRE_THROWS_AS(Board("xxooo"), std::invalid_argument);
+  SECTION("Empty string") {
+      REQUIRE_THROWS_AS(Board(""), std::invalid_argument);
   }
 
-  SECTION("placeholder") {
+  SECTION("String is too short") {
+    REQUIRE_THROWS_AS(Board("xxxxoooo"), std::invalid_argument);
+  }
 
+  SECTION("String is too long") {
+    REQUIRE_THROWS_AS(Board("xxxxxooooo"), std::invalid_argument);
   }
 }
 
+
 TEST_CASE("Boards with no winner") {
-  SECTION("Full board with no winner") {
-    REQUIRE(Board("xxoooxxxo").EvaluateBoard() == BoardState::NoWinner);
+  SECTION("Empty board with special characters") {
+    REQUIRE(Board("!@#$%^&*(").EvaluateBoard() == BoardState::NoWinner);
   }
 
-  SECTION("placeholder") {
+  SECTION("Full board with no winner") {
+    REQUIRE(Board("xoxxxooxo").EvaluateBoard() == BoardState::NoWinner);
+  }
 
+  SECTION("Game in progress, X moved last") {
+    REQUIRE(Board(".xo.x.xo.").EvaluateBoard() == BoardState::NoWinner);
+  }
+
+  SECTION("Game in progress, O moves last") {
+    REQUIRE(Board("xoxoxo...").EvaluateBoard() == BoardState::NoWinner);
   }
 }
